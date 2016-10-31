27c24c1b75270a48819b9204ebe0aaf1c8845f6e
Florian Cassayre <florian.cassayre@gmail.com>
2016-10-31 20:28:32 +0100

    Toujours plus de tests.

--- src/main/java/test/custom/SteganographyAdvancedTests.java
+++ src/main/java/test/custom/SteganographyAdvancedTests.java
@@ -1,16 +1,16 @@
 package test.custom;
 
-import static org.junit.Assert.*;
-
-import main.ImageMessage;
 import main.Steganography;
 import org.junit.Test;
 
-import java.awt.*;
 import java.util.Random;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class SteganographyAdvancedTests
 {
+    private final static Random random = new Random();
 
     @Test
     public void encodeDecodeLSB()
@@ -26,5 +26,39 @@ public class SteganographyAdvancedTests
         assertEquals(Steganography.getLSB(1), true);
         assertEquals(Steganography.getLSB(0x008c261d), true);
         assertEquals(Steganography.getLSB(0x00ecf520), false);
+    }
+
+    @Test
+    public void spiralConversions()
+    {
+        assertArrayEquals(new boolean[] {true}, Steganography.revealSpiralBitArray(new int[][] {{0x005812a7}}));
+        assertArrayEquals(new boolean[] {false}, Steganography.revealSpiralBitArray(new int[][] {{0x0027d582}}));
+
+        assertArrayEquals(new boolean[] {true, false, true, false}, Steganography.revealSpiralBitArray(new int[][] {{1, 0}, {0, 1}}));
+        assertArrayEquals(new boolean[] {false, false, true, true, false, true, false, false, true}, Steganography.revealSpiralBitArray(new int[][] {{0, 0, 1}, {0, 1, 1}, {0, 1, 0}}));
+
+        final boolean[] message = {true, false, true, true, false, false, false, true, true, true, false, true, false, true, false};
+        final boolean[][] encodedSpiral = {
+                {true, false, true, true, false},
+                {true, false, true, false, false},
+                {false, true, true, true, false}
+        };
+
+        final int[][] image = new int[3][5];
+        for(int line = 0; line < image.length; line++)
+        {
+            for(int row = 0; row < image[0].length; row++)
+            {
+                image[line][row] = random.nextInt(1 << 24);
+            }
+        }
+
+        final int[][] imageHidden = Steganography.embedSpiralBitArray(image, message);
+        final boolean[] resultArray = Steganography.revealSpiralBitArray(imageHidden);
+
+        final boolean[][] spiralRaw = Steganography.revealBWImage(imageHidden);
+
+        assertArrayEquals(message, resultArray);
+        assertArrayEquals(spiralRaw, encodedSpiral);
     }
 }
